503f9f95493fdba552553464c1b48600db2cd170
Vishvanath Surwashe <v2018y@gmail.com>
2019-07-29 10:34:48 +0530

    0.1.2 : Now Added The Network Discovery For Eurka

--- src/main/java/com/vany/WebservicesApplication.java
+++ src/main/java/com/vany/WebservicesApplication.java
@@ -2,8 +2,11 @@ package com.vany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebservicesApplication {
 
 	public static void main(String[] args) {
