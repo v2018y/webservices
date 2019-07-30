929aa3e46dd022ff1856dc5c9fac881fdc90deb4
Vishvanath Surwashe <v2018y@gmail.com>
2019-07-30 14:30:52 +0530

    0.1.9 : Resloved Problem Of Accessing of Localhost in zuul api gateway

--- src/main/java/com/vany/WebservicesApplication.java
+++ src/main/java/com/vany/WebservicesApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class WebservicesApplication {
 
 	public static void main(String[] args) {
