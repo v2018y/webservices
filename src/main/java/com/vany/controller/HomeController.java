f0c3c0d56cc9d094ead36e8d8ee38917d2e56478
Vishvanath Surwashe <v2018y@gmail.com>
2019-08-02 10:47:40 +0530

    0.1.13 : Added The Secuirty tag In Pom XML

--- src/main/java/com/vany/controller/HomeController.java
+++ src/main/java/com/vany/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.vany.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,14 @@ public class HomeController {
 	
 	@RequestMapping(value="/")
     public String viewHome() {
+		String username;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+		  username = ((UserDetails)principal).getUsername();
+		} else {
+		  username = principal.toString();
+		}
+		System.out.println("User name : "+ username);
         return "<h1>Yes... Running...</h1>";
     }
 }
